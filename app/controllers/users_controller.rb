d6dce750239a01e1271a9bbbf6a78aae23e18a7b
mostafanour-arc <mostafanour126@gmail.com>
2022-03-11 08:47:06 +0200

    creating user model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render :json => {:user => "user has been create"}, status: :created
+    else
+      render :json => { :errors => @user.errors.full_messages }, status: 422
+    end
+  end
+  private 
+  def user_params
+    params.require(:user).permit(:username)
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe '#validations' do
+    it 'should create a user' do
+      user = build :user
+      expect(user).to be_valid
+      expect(user.username).to eq('Ahmed')
+    end
+    it 'should not create a user without username' do
+      user = build :user, username: nil
+      expect(user).to be_invalid
+      expect(user.errors.messages).to include{"username can't be blank"}
+    end
+  end
+end
