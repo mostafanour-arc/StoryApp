9b4d7f9635ce60892906d11bcba82ffe4ed0abea
mostafanour-arc <mostafanour126@gmail.com>
2022-03-12 18:55:55 +0200

    creating validation tests

--- spec/requests/stories_spec.rb
+++ spec/requests/stories_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Stories", type: :request do
+  let(:user) { create :user }
   describe "GET /index" do
+    subject { get :index, params: { user_id: user.id } }
+    it "returns a success response" do
+      subject
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
